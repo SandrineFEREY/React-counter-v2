7b133c06a4ea374ad6af8ca48112aac30e4550df
Sandrine FEREY <sandrineferey@air-de-sandrine.lan>
2021-11-05 14:30:46 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,61 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState([0]);
+
   return (
-    <div>
-      Hello from <a href="https://www.lereacteur.io">Le Reacteur !</a>
+    <div className="container">
+      <button
+        onClick={() => {
+          if (counter.length < 3) {
+            const newCounter = [...counter];
+            newCounter.push(0);
+            setCounter(newCounter);
+          }
+        }}
+      >
+        Add counter
+      </button>
+
+      {counter.map((elem, index) => {
+        return (
+          <div>
+            <div>
+              <button
+                onClick={() => {
+                  const newCounter = [...counter];
+                  newCounter[index] = newCounter[index] - 1; //ou newCounter[index]--
+                  setCounter(newCounter);
+                }}
+              >
+                -
+              </button>
+
+              <span>{elem}</span>
+
+              <button
+                onClick={() => {
+                  const newCounter = [...counter];
+                  newCounter[index]++;
+                  setCounter(newCounter);
+                }}
+              >
+                +
+              </button>
+            </div>
+            <button
+              onClick={() => {
+                const newCounter = [...counter];
+                newCounter[index] = 0;
+                setCounter(newCounter);
+              }}
+            >
+              Reset
+            </button>
+          </div>
+        );
+      })}
     </div>
   );
 }
